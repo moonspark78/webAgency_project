3c026c2eca8f0091f6cb40552bd355c8a6396d32
Omar Jabour <jabour.omar@hotmail.com>
2026-07-07 22:12:41 +0200

    init .././././

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Rubik } from "next/font/google";
 import "./globals.css";
+
+const font = Rubik({
+  weight: ["300", "400", "500", "600", "700", "800" ,"900"],
+  subsets: ["latin"],
+})
 
 
 export const metadata: Metadata = {
@@ -16,9 +21,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body
+        className={`${font.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
