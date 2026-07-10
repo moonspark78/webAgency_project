37dcf1bbf69c43268c7f1e9bba4e37a84c6fd49b
Omar Jabour <jabour.omar@hotmail.com>
2026-07-10 11:14:56 +0200

    working global.css ./././././

---
+++ components/Helper/ThemeToggler.tsx
@@ -0,0 +1,10 @@
+import {useState, useEffect} from 'react'
+import {useTheme} from 'next-themes'
+
+const ThemeToggler = () => {
+  return (
+    <div>ThemeToggler</div>
+  )
+}
+
+export default ThemeToggler
