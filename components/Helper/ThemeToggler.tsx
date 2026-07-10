5930ebb07d09dd21c0440ee53829e162d616577d
Omar Jabour <jabour.omar@hotmail.com>
2026-07-10 11:17:36 +0200

    working ThemeToggler ././././

--- components/Helper/ThemeToggler.tsx
+++ components/Helper/ThemeToggler.tsx
@@ -4,6 +4,8 @@ import {useTheme} from 'next-themes'
 
 const ThemeToggler = () => {
     const [mounted, setMounted] = useState(false)
+
+    const {theme, setTheme, systemTheme} = useTheme()
   return (
     <div>ThemeToggler</div>
   )
