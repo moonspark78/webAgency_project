05c18f932a197befe8cfae683ea1d220ce706539
Omar Jabour <jabour.omar@hotmail.com>
2026-07-11 12:05:54 +0200

    working ThemeToggler ./././././.

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
     >
       <body
         className={`${font.className} antialiased`}>
