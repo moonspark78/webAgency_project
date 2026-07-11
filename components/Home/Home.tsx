d24bee7a07683987c629f611c74b060a43b4b5b0
Omar Jabour <jabour.omar@hotmail.com>
2026-07-11 12:05:07 +0200

    working ThemeToggler ././././././

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import ThemeToggler from '../Helper/ThemeToggler'
 
 const Home = () => {
   return (
-    <div className="overflow-hidden">Home</div>
+    <div className="overflow-hidden">
+      <ThemeToggler />
+    </div>
   )
 }
     
