6a42734532b06320f36f88f749e2e26888f37ec1
Omar Jabour <jabour.omar@hotmail.com>
2026-07-12 12:33:35 +0200

    working ResponsiveNav

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -5,7 +5,7 @@ import { NAVLINKS } from '@/constant/constant'
 const Nav = () => {
   return (
     <div className="transition-all duration-200 h-[12vh] z-100 fixed w-full">
-      <div className="flex justify-between items-center h-full ">
+      <div className="flex justify-between items-center h-full w-[90%] xl:w-[80%] mx-auto">
 
       </div>
     </div>
