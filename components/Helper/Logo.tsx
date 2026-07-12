50e97d7640756292cef50f8a40fa7ccd3d988bec
Omar Jabour <jabour.omar@hotmail.com>
2026-07-12 12:46:01 +0200

    working on Logo ./././

--- components/Helper/Logo.tsx
+++ components/Helper/Logo.tsx
@@ -4,7 +4,7 @@ import { CgWebsite } from 'react-icons/cg'
 const Logo = () => {
   return (
     <div className="flex items-center space-x-2">
-        <div>
+        <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center flex-col">
             <CgWebsite className="w-6 h-6 text-black"/>
         </div>
     </div>
