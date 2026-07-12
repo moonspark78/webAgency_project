320e8b67bcb5dbf77a55495913941af524ba035d
Omar Jabour <jabour.omar@hotmail.com>
2026-07-12 12:47:01 +0200

    working on Logo ././

--- components/Helper/Logo.tsx
+++ components/Helper/Logo.tsx
@@ -7,7 +7,7 @@ const Logo = () => {
         <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center flex-col">
             <CgWebsite className="w-6 h-6 text-black"/>
         </div>
-        <h1>
+        <h1 className="text-xl hidden sm:block md:text-2xl text-white font-bold">
             WebDev
         </h1>
     </div>
