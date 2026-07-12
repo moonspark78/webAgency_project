400c881b17165ca716b43951007484dfbedf7750
Omar Jabour <jabour.omar@hotmail.com>
2026-07-12 12:45:18 +0200

    working on Logo ./././.

--- components/Helper/Logo.tsx
+++ components/Helper/Logo.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { CgWebsite } from 'react-icons/cg'
 
 const Logo = () => {
   return (
     <div className="flex items-center space-x-2">
-
+        <div>
+            <CgWebsite className="w-6 h-6 text-black"/>
+        </div>
     </div>
   )
 }
