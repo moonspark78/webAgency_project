c1271215a12abcf84fd68dc815c1cf47ad06864b
Omar Jabour <jabour.omar@hotmail.com>
2026-07-12 12:49:15 +0200

    working on Logo :art:

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import Link from 'next/link'
-import { NAVLINKS } from '@/constant/constant'
+import Logo from '../../Helper/Logo';
 
 const Nav = () => {
   return (
     <div className="transition-all duration-200 h-[12vh] z-100 fixed w-full">
       <div className="flex justify-between items-center h-full w-[90%] xl:w-[80%] mx-auto">
         {/* Logo */}
+        <Logo/>
       </div>
     </div>
   );
