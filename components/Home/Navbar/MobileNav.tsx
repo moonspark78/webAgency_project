10783bbfda865e05a596a69cd1ebee01e6e4ff8a
Omar Jabour <jabour.omar@hotmail.com>
2026-07-15 19:40:20 +0200

    working on Nav ././././   :art:

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NAVLINKS } from '../../../constant/constant.ts'
+import { NAVLINKS } from '../../../constant/constant'
 import Link from 'next/link'
 
 const MobileNav = () => {
@@ -11,7 +11,7 @@ const MobileNav = () => {
       <div className="text-white fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%]
       sm:w-[60%] bg-blue-900 space-y-6 z-1050">
         {NAVLINKS.map((link) => {
-          return <Link key={link.id}></Link>
+          return <Link key={link.id} href={link.url}></Link>
         })}
       </div>
     </div>
