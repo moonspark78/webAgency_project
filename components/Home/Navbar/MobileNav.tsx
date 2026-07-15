bbba0f55272fb69bb4094cc18ec9a4591cb163bc
Omar Jabour <jabour.omar@hotmail.com>
2026-07-16 00:50:48 +0200

    working on MobileNav /./././.  :art:

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -21,7 +21,7 @@ const MobileNav = () => {
           );
         })}
         {/* Close icon */}
-        <CgClose className='absolute top-[0.7rem] right-[1.4rem]'/>
+        <CgClose className='absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6'/>
       </div>
     </div>
   )
