530f783d88d64f1fcc485b8ac12e482bb911dd76
Omar Jabour <jabour.omar@hotmail.com>
2026-07-16 00:53:34 +0200

    working on MobileNav /./././  :art:

--- components/Home/Navbar/ResponsiveNav.tsx
+++ components/Home/Navbar/ResponsiveNav.tsx
@@ -1,14 +1,18 @@
 "use client"
 
+import { useState } from "react"
+import MobileNav from "./MobileNav"
 import Nav from "./Nav"
 
 
 const ResponsiveNav = () => {
+  const [showNav, setShowNav] = useState(false)
 
 
   return (
     <div>
       <Nav/>
+      <MobileNav/>
     </div>
   )
 }
