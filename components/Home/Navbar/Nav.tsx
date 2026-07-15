8aeda77d72a2dc1c18d9ee9872e090c4b7e77f50
Omar Jabour <jabour.omar@hotmail.com>
2026-07-15 19:15:43 +0200

    working on Nav ./././././.././.   :art:

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -23,7 +23,8 @@ const Nav = () => {
   }, []);
 
   return (
-    <div className={`transition-all duration-200 h-[12vh] z-100 fixed w-full bg-black`}>
+    <div className={`transition-all ${
+    navBg ? "dark:bg-gray-900"} duration-200 h-[12vh] z-100 fixed w-full bg-black`}>
       <div className="flex justify-between items-center h-full w-[90%] xl:w-[80%] mx-auto">
         {/* Logo */}
         <Logo/>
