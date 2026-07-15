c4001359659e7ce7a2f0c6beb29f9a8e864beedb
Omar Jabour <jabour.omar@hotmail.com>
2026-07-15 19:33:59 +0200

    working on Nav ./././././   :art:

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -6,7 +6,8 @@ const MobileNav = () => {
       {/* Overlay */}
       <div className="fixed inset-0 transform transition-all duration-500 z-1002 bg-black opacity-70 w-full h-screen"></div>
       {/* Navlinks */}
-      <div className="text-white fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%]">
+      <div className="text-white fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%]
+      sm:w-[60%] bg-blue-900 space-y-6 z-1050">
 
       </div>
     </div>
